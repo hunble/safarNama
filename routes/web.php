ed517dd5111f48cb07215b3ab93504d36d5f8c8c
Hunble Dhillon <hunbledhillon@gmail.com>
2017-09-30 20:04:26 +0500

    New Laravel Completed First Half

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,25 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+*/
+
+Route::get('/','pagesController@index');
+Route::get('/about','pagesController@about');
+Route::get('/services','pagesController@services');
+
+
+Route::resource('posts','PostsController');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

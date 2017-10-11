9af7ebc9622b1668bcac3816e6d6fb9abd655270
Hunble Dhillon <hunbledhillon@gmail.com>
2017-10-12 01:29:55 +0500

    happens

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/png" href="http://bloomingfounders.com/wp-content/uploads/2015/09/cropped-favicon.jpg
 
-        <title>Laravel</title>
+		<title>{{ config('app.name', 'Travelly') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">

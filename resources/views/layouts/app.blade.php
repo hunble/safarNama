f930ec7d61661a90bd061dbf8ed9c98df9c25359
Hunble Dhillon <hunbledhillon@gmail.com>
2017-10-12 01:28:09 +0500

    happens

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Travelly') }}</title>
 
     <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">

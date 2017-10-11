04d1cacd7c2b359eee3df5de857fd36b28e3e215
Hunble Dhillon <hunbledhillon@gmail.com>
2017-10-12 00:00:56 +0500

    happens

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -14,7 +14,7 @@ class CommentsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth',['except' => ['index', 'show','store']]);
     }
 
 	 

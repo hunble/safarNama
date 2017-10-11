89aff4a95d163cddabed21aee46d16b7c744edc9
Hunble Dhillon <hunbledhillon@gmail.com>
2017-10-12 01:26:46 +0500

    happens

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/png" href="http://bloomingfounders.com/wp-content/uploads/2015/09/cropped-favicon.jpg"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">

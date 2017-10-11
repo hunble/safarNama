daabf66b897c06f4cb9c232963f718af516668ae
Hunble Dhillon <hunbledhillon@gmail.com>
2017-10-12 01:31:55 +0500

    happens

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="shortcut icon" type="image/png" href="http://bloomingfounders.com/wp-content/uploads/2015/09/cropped-favicon.jpg
+		<link rel="shortcut icon" type="image/png" href="http://bloomingfounders.com/wp-content/uploads/2015/09/cropped-favicon.jpg"/>
 
 		<title>{{ config('app.name', 'Travelly') }}</title>
 

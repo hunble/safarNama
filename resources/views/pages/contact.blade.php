f15410b6087452c6708fedb3aece0ad04a87ed80
Hunble Dhillon <hunbledhillon@gmail.com>
2017-12-10 20:21:02 +0500

    after 2 month

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,8 +6,8 @@
 		<p><strong>Muhammad Hunble Dhillon</strong><br />
 			<ul>
 				<li>Student Computer Science, FAST NUCES Lahore</li>
-				<li><a href="email:hunbledhillon@gmail.com" >hunbledhillon@gmail.com</a></li>
-				<li><a href="email:l134118@lhr.nu.edu.pk">l134118@lhr.nu.edu.pk</a></li>
+				<li><a href="mailto:hunbledhillon@gmail.com" >hunbledhillon@gmail.com</a></li>
+				<li><a href="mailto:l134118@lhr.nu.edu.pk">l134118@lhr.nu.edu.pk</a></li>
 				<li><a href='tel:+923134124730'>+92-313-4124-730</a></li>
 			</ul>
 	<div>
